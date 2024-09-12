73852f9343e4f1540044ff033235d2a95ff27855
PROST Vincent (ENGIE SA) <rb6283@engie.com>
2024-09-12 11:11:01 +0200

    save work

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import './App.css';
+import ChessBoardFromFEN from './ChessBoardFromFEN.js';
+import { Chessboard } from 'react-chessboard';
 
 function App() {
   const [selectedFile, setSelectedFile] = useState(null);
   const [previewImage, setPreviewImage] = useState(null);
-  const [responseImage, setResponseImage] = useState(null);
+  //const [responseImage, setResponseImage] = useState(null);
   const [uploadSuccess, setUploadSuccess] = useState(false);
   const [uploadError, setUploadError] = useState(null);
   const [loading, setLoading] = useState(false); // State for loading indicator
+  const [piecesPosition, setPiecesPosition] = useState(null); // State to hold pieces position data
+  const [fen, setFen] = useState(null); // Position de départ
 
+
+  const canvasRef = useRef(null); // Reference to the canvas element
 
   const handleFileChange = (event) => {
     const file = event.target.files[0];
@@ -23,33 +29,64 @@ function App() {
     setLoading(true); // Start loading
 
     try {
-      const response = await axios.post('https://api.vincentprost.com/upload', formData, {
+      const response = await axios.post('http://127.0.0.1:5000/upload', formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
-        responseType: 'blob',
       });
       console.log(response)
-      
+
       if (response.status === 200) {
-        const imageUrl = URL.createObjectURL(response.data);
-        setResponseImage(imageUrl);
+        const piecesPos = response.data.pieces_boxes;
+        console.log(response.data)
+
+        setFen(response.data.fen);
+        setPiecesPosition(piecesPos);
+
         setUploadSuccess(true);
         setUploadError(null);
       }
     } catch (error) {
       setUploadError('Failed to upload image');
       setUploadSuccess(false);
-      setResponseImage(null);
+      setFen(null);
     } finally {
-      setLoading(false); 
-      if(selectedFile){
+      setLoading(false);
+      if (selectedFile) {
         const fileUrl = URL.createObjectURL(selectedFile);
         setPreviewImage(fileUrl);
       }
-      
+
     }
   };
+
+  useEffect(() => {
+    if (previewImage && piecesPosition && canvasRef.current) {
+      const canvas = canvasRef.current;
+      const ctx = canvas.getContext('2d');
+
+      // Reset canvas size and clear previous drawings
+      const img = new Image();
+      img.src = previewImage;
+      img.onload = () => {
+        canvas.width = img.width;
+        canvas.height = img.height;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        // Draw the image on the canvas
+        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+
+        // Draw the boxes over the image
+        piecesPosition.forEach(([x, y, w, h]) => {
+          ctx.strokeStyle = 'red';
+          ctx.lineWidth = 2;
+          ctx.strokeRect(x, y, w, h); // Draw each box
+        });
+
+        console.log("image size: ", img.width, img.height);
+      };
+    }
+  }, [previewImage, piecesPosition]);
 
   return (
     <div className="App">
@@ -62,17 +99,31 @@ function App() {
         {loading && <p>Computing...</p>} {/* Loading indicator */}
         {uploadSuccess && <p>Image computed successfully</p>}
 
+
         <div className="image-container">
 
           {previewImage && (
-              <div className="image-wrapper">              
-                <img src={previewImage} alt="Uploaded file preview" style={{ width: '300px', marginTop: '20px' }} />
+            <>
+              <div className="image-wrapper">
+                {//<img src={previewImage} alt="Uploaded file preview" style={{ width: '300px', marginTop: '20px', display: 'block', maxWidth: '100%' }} />
+                }
+
               </div>
-            )}
-          {responseImage && (
-            <div className="image-wrapper">
-              <img src={responseImage} alt="Received from server" style={{ width: '300px', marginTop: '20px' }} />
-            </div>
+              <canvas
+                ref={canvasRef}
+                style={{ width: '300px', marginTop: '20px', display: 'block', maxWidth: '100%' }}
+              />
+            </>
+          )}
+
+          {fen && (
+            <>
+              <div className="image-wrapper">
+                {//<img src={responseImage} alt="Received from server" style={{ width: '300px', marginTop: '20px' }} />
+                }
+                < ChessBoardFromFEN fen={fen} />
+              </div>
+            </>
           )}
         </div>
         {uploadError && <p>{uploadError}</p>}
